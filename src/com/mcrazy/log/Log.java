3679ea91b51f3ef2e1e407adc90e3a717ed33559
chao <lmc.0217@163.com>
2018-01-23 17:51:55 +0800

    first commit code

---
+++ src/com/mcrazy/log/Log.java
@@ -0,0 +1,30 @@
+package com.mcrazy.log;
+
+import java.io.File;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Log日记处理
+ * 
+ * @author liu mengchao
+ * 
+ */
+public final class Log {
+	public static final String PATH_LOG = "/home/workspace/MQTTServer/logs";
+	public static final String PATH_CONF = "/home/workspace/MQTTServer/conf";
+	
+	private Log(){
+		
+	}
+	
+	static{
+		StringBuilder builder = new StringBuilder();
+		String filepath = builder.append(PATH_CONF).append(File.separator)
+				.append("log4j.properties").toString();
+		PropertyConfigurator.configure(filepath);
+	}
+	public static Logger err = LoggerFactory.getLogger(Log.class); // log
+}
